ca6725d4f2b5f73e27743bc1a8782b48b9b9c72b
Zack <99695700+Zack4U@users.noreply.github.com>
2023-04-28 17:58:47 -0500

    Cretion of accepted/rejected adviser form notifications

--- src/controllers/adviser-form.controller.ts
+++ src/controllers/adviser-form.controller.ts
@@ -1,5 +1,6 @@
 // Uncomment these imports to begin using these cool features!
 
+import {authenticate} from '@loopback/authentication';
 import {service} from '@loopback/core';
 import {repository} from '@loopback/repository';
 import {
@@ -7,25 +8,28 @@ import {
   getModelSchemaRef,
   post,
   requestBody,
-  response
+  response,
 } from '@loopback/rest';
 import {GeneralConfig} from '../config/general.config';
+import {SecurityConfig} from '../config/security.config';
 import {AdviserForm, GeneralSystemVariables} from '../models';
 import {GeneralSystemVariablesRepository} from '../repositories';
 import {NotificationsService} from '../services';
 
-
 // import {inject} from '@loopback/core';
-
 
 export class AdviserFormController {
   constructor(
-    @repository (GeneralSystemVariablesRepository)
+    @repository(GeneralSystemVariablesRepository)
     public variablesRepository: GeneralSystemVariablesRepository,
     @service(NotificationsService)
     public notificationService: NotificationsService,
   ) {}
 
+  @authenticate({
+    strategy: 'auth',
+    options: [SecurityConfig.menuAdviserId, SecurityConfig.createAction],
+  })
   @post('/send-message-advisor-request')
   @response(200, {
     description: 'Sending a request for an advisor through the public form',
@@ -75,5 +79,71 @@ export class AdviserFormController {
     } catch (err) {
       throw new HttpErrors[500]('Server error when sending message');
     }
+  }
+
+  @authenticate({
+    strategy: 'auth',
+    options: [SecurityConfig.menuAdviserId, SecurityConfig.createAction],
+  })
+  @post('/adviser-form-rejected')
+  @response(200, {
+    description: 'Rejected adviser request',
+  })
+  async rejectedAdviserForm(
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(AdviserForm),
+        },
+      },
+    })
+    data: AdviserForm,
+  ): Promise<boolean> {
+    let subject = 'Answer: Adviser Application';
+    let content = `Hi ${data.fullName}, <br /> Your application to be an adviser for our company Akinmueble has been REJECTED}`;
+    let contactData = {
+      destinyEmail: data.email,
+      destinyName: data.fullName,
+      emailSubject: subject,
+      emailBody: content,
+    };
+    let sent = this.notificationService.sendNotification(
+      contactData,
+      GeneralConfig.urlNotificationsEmail,
+    );
+    return sent;
+  }
+
+  @authenticate({
+    strategy: 'auth',
+    options: [SecurityConfig.menuAdviserId, SecurityConfig.createAction],
+  })
+  @post('/adviser-form-accepted')
+  @response(200, {
+    description: 'Accepted adviser request',
+  })
+  async acceptedAdviserForm(
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(AdviserForm),
+        },
+      },
+    })
+    data: AdviserForm,
+  ): Promise<boolean> {
+    let subject = 'Answer: Adviser Application';
+    let content = `Hi ${data.fullName}, <br /> Your application to be an adviser for our company Akinmueble has been ACCEPTED}`;
+    let contactData = {
+      destinyEmail: data.email,
+      destinyName: data.fullName,
+      emailSubject: subject,
+      emailBody: content,
+    };
+    let sent = this.notificationService.sendNotification(
+      contactData,
+      GeneralConfig.urlNotificationsEmail,
+    );
+    return sent;
   }
 }
